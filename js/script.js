96be688d8b62b5448e28e1868218348ef4ea7ec5
RuslanTurchaninov <72912270+RuslanTurchaninov@users.noreply.github.com>
2020-11-07 19:29:31 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+let neutral = false;
+let sandwichLine = document.getElementsByClassName("sandwich-line");
+let menu = document.getElementById("header-menu");
+
+function openMobileMenu() {
+  if (neutral === false) {
+    menu.classList.add("active");
+    sandwichLine[0].classList.add("active");
+    sandwichLine[1].classList.add("active");
+    sandwichLine[2].classList.add("active");
+    neutral = true;
+  } else {
+    menu.classList.remove("active");
+    sandwichLine[0].classList.remove("active");
+    sandwichLine[1].classList.remove("active");
+    sandwichLine[2].classList.remove("active");
+    neutral = false;
+  }
+}
